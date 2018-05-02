b577b6682087517b234dc7c53a83f07a8975477a
echobin23 <league@WTS14-4.attlocal.net>
2018-05-01 20:00:11 -0700

    Intro to Constructers ip

---
+++ src/Platypus.java
@@ -0,0 +1,9 @@
+
+public class Platypus {
+	private String name;
+
+	public void sayHi() {
+		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+
+	}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,9 @@
+
+public class PlatypusRunner {
+	static Platypus platypus = new Platypus();
+
+	public static void main(String[] args) {
+		platypus.sayHi();
+
+	}
+}
